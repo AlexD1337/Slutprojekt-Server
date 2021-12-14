121a1320ca44c6582bf93ca85450f73e5a9c9b6c
Alex Danil <70381167+AlexD1337@users.noreply.github.com>
2021-12-14 15:26:37 +0100

    Add files via upload

---
+++ Code/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// set port
+const PORT = process.env.PORT || 3000;
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+
+    // listen for requests
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}.`);
+    });
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+   
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+require("./app/routes/tutorial.routes")(app);
+
+
+
+
